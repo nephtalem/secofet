8a63651d6121790e9eb760cb9f63064091cd439c
Nephtalem Woldeyohanes <nephtalemwoldeyohanes@gmail.com>
2025-06-19 01:48:59 +0300

    resend

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import { ContactEmail } from "@/app/emails/ContactEmail";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export async function POST(req: NextRequest) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const { name, email, message } = await req.json();
     if (!name || !email || !message) {
